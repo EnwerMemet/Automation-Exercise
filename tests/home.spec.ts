888b698647479d8a9cbbe07dc0a914a485078324
enwer <tewbejan@gmail.com>
2025-05-06 19:37:56 +0200

    removed an extra file

--- tests/home.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-
-test('Automation Exercise homepage ', async ({ page }) => {
-  await page.goto('/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Automation Exercise/);
-});
